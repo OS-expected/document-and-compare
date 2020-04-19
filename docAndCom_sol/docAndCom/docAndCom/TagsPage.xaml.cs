159f16ad9b6fac82b37ccaa7fea1bd4704d0d3f2
Paweł Idzikowski <iddziku@gmail.com>
2020-04-19 18:26:39 +0200

    Update TagsPage

--- docAndCom_sol/docAndCom/docAndCom/TagsPage.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/TagsPage.xaml.cs
@@ -19,6 +19,61 @@ namespace docAndCom
             DisplayTags();
         }
 
+        private async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var result = await DisplayAlert("Confirmation", "Are you sure, you want to delete that tag?", "Yes", "No");
+
+            if(result == true)
+            {
+                var button = sender as Button;
+                var tag = button.BindingContext as Tag;
+
+                using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+                {
+                    conn.Delete<Tag>(tag.Id);
+                }
+
+                DisplayTags();
+            }
+        }
+
+        private async void ToolbarItem_AddTag_Activated(object sender, EventArgs e)
+        {
+            var result = await DisplayPromptAsync("Tag", "Specify name of the tag(3-20 characters)",
+                "Create", "Abort", null, 20);
+
+            if (string.IsNullOrEmpty(result) == false && result.Length >= 3)
+            {
+                Tag tag = new Tag()
+                {
+                    Name = result
+
+                };
+
+                using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+                {
+                    conn.CreateTable<Tag>();
+
+                    var numberOfRows = conn.Insert(tag);
+
+                    if (numberOfRows > 0)
+                    {
+                        await DisplayAlert("Success", "Tag successfuly added.", "Great");
+                    }
+                    else
+                    {
+                        await DisplayAlert("Failure", "Tag not inserted.", "Ok");
+                    }
+                }
+                DisplayTags();
+            } else if (result != null && result.Length < 3)
+            {
+                await DisplayAlert("Failure", "Tag not added. Not enough characters.", "Ok");
+            }
+
+            // Navigation.PushAsync(new NewTagPage());
+        }
+
         private void DisplayTags()
         {
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
@@ -27,9 +82,16 @@ namespace docAndCom
 
                 var tags = conn.Table<Tag>().ToList();
 
+                if(tags.Count == 0)
+                {
+                    emptyTagsLabel.IsVisible = true;
+                } else
+                {
+                    emptyTagsLabel.IsVisible = false;
+                }
+
                 tagsListView.ItemsSource = tags;
             }
-
         }
     }
 }
