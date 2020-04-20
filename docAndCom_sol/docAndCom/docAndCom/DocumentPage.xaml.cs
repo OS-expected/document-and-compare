5688e7dfa047ae91734cac93ac864f4cc534cd90
Paweł Idzikowski <iddziku@gmail.com>
2020-04-20 14:02:19 +0200

    Add media, takephoto, gallery and calendar is working

--- docAndCom_sol/docAndCom/docAndCom/DocumentPage.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/DocumentPage.xaml.cs
@@ -1,21 +1,65 @@
-﻿using System;
-
+﻿using docAndCom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Plugin.Calendar.Models;
 
 namespace docAndCom
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DocumentPage : ContentPage
     {
+        public EventCollection Events { get; set; }
+
         public DocumentPage()
         {
             InitializeComponent();
+
+            InitEventsInCalendar();
         }
 
         private async void ToolbarItem_Document_Activated(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NewDocumentPage());
+        }
+
+        private void InitEventsInCalendar()
+        {
+            Events = new EventCollection();
+
+            SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH);
+
+            conn.CreateTable<Photo>();
+
+            var photos = conn.Table<Photo>().ToList();
+
+            var days = (from table in conn.Table<Photo>()
+                  select table.CreatedOn).Distinct().ToList();
+
+            conn.Dispose();
+
+            foreach (var day in days)
+            {
+                List<EventModel> list = new List<EventModel>();
+
+                foreach (var photoObj in photos)
+                {
+                    if(photoObj.CreatedOn == day)
+                    {
+                        list.Add(new EventModel()
+                        {
+                            Tag = "Test",
+                            ImagePath = photoObj.Path
+                        });
+                    }
+                }
+
+                Events.Add(day, list);
+            }
+
+            calendarRef.Events = Events;
         }
     }
 }
--- docAndCom_sol/docAndCom/docAndCom/NewDocumentPage.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/NewDocumentPage.xaml.cs
@@ -1,10 +1,9 @@
-﻿using Plugin.Media;
+﻿using docAndCom.Models;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,30 +13,41 @@ namespace docAndCom
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewDocumentPage : ContentPage
     {
+        private string pathToImage = "";
+
+        private string pickedTag = "";
+
+        // private string pickedDate = "";
+
         public NewDocumentPage()
         {
             InitializeComponent();
 
-            FeedTags();
+            FeedTagPicker();
         }
 
         async void TakePhoto_Clicked(object sender, System.EventArgs e)
         {
             if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
             {
-                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
                 return;
             }
+
+            var now = DateTime.UtcNow;
+
             var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
             {
-                Directory = "Sample",
-                Name = "test.jpg"
+                Directory = "docAndComparePhotos",
+                Name = $"dac_{now:dd-MM-yyyy}_{now:HH_mm}.jpg"
             });
 
             if (file == null)
                 return;
 
             await DisplayAlert("File Location", file.Path, "OK");
+
+            pathToImage = file.Path.ToString();
 
             image.Source = ImageSource.FromStream(() =>
             {
@@ -47,7 +57,25 @@ namespace docAndCom
             });
         }
 
-        private void FeedTags()
+        private async void Gallery_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("no upload", "picking a photo is not supported", "ok");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() => file.GetStream());
+
+            pathToImage = file.Path.ToString();
+        }
+
+        private void FeedTagPicker()
         {
             using(SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
             {
@@ -67,15 +95,59 @@ namespace docAndCom
                 }
 
                 var tagsList = new List<string>();
-                foreach (var tag in tagsList)
+                foreach (var tag in tags)
                 {
-                    if(tagsList.Contains(tag) == false)
+                    if(tagsList.Contains(tag.Name) == false)
                     {
-                        tagsList.Add(tags[0].Name);
+                        tagsList.Add(tag.Name);
                     }
                 }
 
                 tagPicker.ItemsSource = tagsList;
+            }
+        }
+
+        private async void DocumentBtn_Clicked(object sender, EventArgs e)
+        {
+            pickedTag = tagPicker.SelectedItem.ToString();
+
+            if (string.IsNullOrEmpty(pickedTag))
+            {
+                await DisplayAlert("Operation aborted", "Tag not picked.", "Ok");
+                return;
+            }
+
+            SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH);
+
+            var id = conn.Table<Tag>().SingleOrDefault(t => t.Name == pickedTag).Id;
+
+            if (id <= 0)
+            {
+                conn.Dispose();
+                await DisplayAlert("Operation aborted", "Tag does not exist in database.", "Ok");
+                return;
+            }
+
+            conn.CreateTable<Photo>();
+
+            Photo photo = new Photo()
+            {
+                Path = pathToImage,
+                CreatedOn = documentDatePicker.Date,
+                TagId = id
+            };
+
+            var numberOfRows = conn.Insert(photo);
+
+            conn.Dispose();
+
+            if (numberOfRows > 0)
+            {
+                await DisplayAlert("Success", "Image successfuly documented in the app.", "Great!");
+            }
+            else
+            {
+                await DisplayAlert("Failure", "Image not documented in the app, try again!", "Ok");
             }
         }
     }
