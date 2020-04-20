ee8add951d67530b8a1aeb46085bde0b444da02d
Paweł Idzikowski <iddziku@gmail.com>
2020-04-20 14:01:54 +0200

    Create EventModel.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/Models/EventModel.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Plugin.Calendar.Models;
+
+namespace docAndCom.Models
+{
+    class EventModel
+    {
+        public string Tag { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
