b43d25606238e4031b675fb9be179eee60611c3c
Paweł Idzikowski <iddziku@gmail.com>
2020-04-20 10:37:11 +0200

    Create DocumentPage, add basic picker and calendar

---
+++ docAndCom_sol/docAndCom/docAndCom/DocumentPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace docAndCom
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DocumentPage : ContentPage
+    {
+        public DocumentPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Document_Activated(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewDocumentPage());
+        }
+    }
+}
