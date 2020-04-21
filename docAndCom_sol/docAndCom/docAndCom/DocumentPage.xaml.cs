0a58600d73bebc893608a893d2fddce342019ba2
Paweł Idzikowski <iddziku@gmail.com>
2020-04-21 16:39:37 +0200

    Add definition of hard delete

--- docAndCom_sol/docAndCom/docAndCom/DocumentPage.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/DocumentPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using docAndCom.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -30,6 +31,54 @@ namespace docAndCom
             string path = ((Button)sender).BindingContext as string;
 
             DependencyService.Get<IFileOpener>().OpenFileByGivenPath(path);
+        }
+
+        private async void HardDelBtn_Clicked(object sender, EventArgs e)
+        {
+            string hardDelData = ((Button)sender).BindingContext as string;
+            string[] arr = hardDelData.Split(new[] { '|' }, 2); // 0 -> path, 1 -> tag
+
+            bool answer = await DisplayAlert("Are you sure?", $"This operation will permanently remove documented image(of {arr[1]} tag) from storage device.", "Yes", "No");
+
+            if(answer == true)
+            {
+                using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+                {
+                    var tagName = arr[1];
+                    var tag = conn.Table<Tag>().SingleOrDefault(t => t.Name == tagName);
+
+                    if (tag == null)
+                    {
+                        await DisplayAlert("Failure", "Tag not found. Operation aborted.", "Ok");
+                        return;
+                    }
+
+                    var photoPath = arr[0];
+                    var photo = conn.Table<Photo>().SingleOrDefault(p => p.Path == photoPath && p.TagId == tag.Id);
+
+                    if (photo == null)
+                    {
+                        await DisplayAlert("Failure", "Operation aborted. Reference to the image not found.", "Ok");
+                        return;
+                    }
+
+                    var res = conn.Delete<Photo>(photo.Id);
+
+                    if (File.Exists(photo.Path))
+                    {
+                        File.Delete(photo.Path);
+                    }
+
+                    if (res > 0)
+                    {
+                        await DisplayAlert("Success", "Clear reference operation succeded.", "Ok");
+                        InitEventsInCalendar();
+                    } else
+                    {
+                        await DisplayAlert("Failure", "Hard delete operation failed.", "Ok");
+                    }
+                }
+            }
         }
 
         private async void ClearRefBtn_Clicked(object sender, EventArgs e)
