ac2f9e2f4df3a0164e7a8c01c1ef6980f46a7dad
Paweł Idzikowski <iddziku@gmail.com>
2020-04-22 01:07:20 +0200

    Edit document, add DocumentVM

--- docAndCom_sol/docAndCom/docAndCom/Models/Document.cs
+++ docAndCom_sol/docAndCom/docAndCom/Models/Document.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using SQLite;
+using System;
 
 namespace docAndCom.Models
 {
     public class Document
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+
+        public string FileName { get; set; }
 
         public string Path { get; set; }
 
         public DateTime GeneratedOn { get; set; }
-
-        public bool IsExisting { get; set; }
     }
 }
---
+++ docAndCom_sol/docAndCom/docAndCom/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace docAndCom.ViewModels
+{
+    public class DocumentViewModel
+    {
+        public int Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Path { get; set; }
+
+        public string GeneratedOn { get; set; }
+
+        public bool IsExisting { get; set; }
+    }
+}
