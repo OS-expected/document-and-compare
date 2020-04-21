ac2bf9918202b707c744bc70089b1a3571eaad2d
Paweł Idzikowski <iddziku@gmail.com>
2020-04-22 01:06:46 +0200

    Create IFileSaver.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/IFileSaver.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace docAndCom
+{
+    public interface IFileSaver
+    {
+        void SaveAsPdf(string filename, string contentType, MemoryStream stream);
+    }
+}
