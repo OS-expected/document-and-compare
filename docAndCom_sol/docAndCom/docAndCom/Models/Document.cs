8ffd451dfdb577d1d34599f249583fc3ce32289c
Paweł Idzikowski <iddziku@gmail.com>
2020-04-21 21:55:50 +0200

    Create Document model

---
+++ docAndCom_sol/docAndCom/docAndCom/Models/Document.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace docAndCom.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+
+        public string Path { get; set; }
+
+        public DateTime GeneratedOn { get; set; }
+    }
+}
