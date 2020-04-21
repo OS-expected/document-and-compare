2675d516de7de6a1c0ff06cf2452758724cd39d9
Paweł Idzikowski <iddziku@gmail.com>
2020-04-22 01:22:03 +0200

    Add SaveAsPdf method to AndroidCommandsAct

--- docAndCom_sol/docAndCom/docAndCom.Android/AndroidCommandsActivity.cs
+++ docAndCom_sol/docAndCom/docAndCom.Android/AndroidCommandsActivity.cs
@@ -1,15 +1,21 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
+using Android.Support.V4.App;
 using Android.Support.V4.Content;
 using Android.Webkit;
 using docAndCom.Droid;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 [assembly: Xamarin.Forms.Dependency(typeof(AndroidCommandsActivity))]
 namespace docAndCom.Droid
 {
     [Activity(Label = "AndroidCommandsActivity")]
-    public class AndroidCommandsActivity : Activity, IFileOpener
+    public class AndroidCommandsActivity : Activity, IFileOpener, IFileSaver
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,6 +37,38 @@ namespace docAndCom.Droid
             var chooserIntent = Intent.CreateChooser(intent, "Open file");
             chooserIntent.SetFlags(ActivityFlags.ClearWhenTaskReset | ActivityFlags.NewTask);
             Application.Context.StartActivity(chooserIntent);
+        }
+
+        public void SaveAsPdf(string fileName, String contentType, MemoryStream stream)
+        {
+            string exception = string.Empty;
+            string root = null;
+
+            if (ContextCompat.CheckSelfPermission(Application.Context, Manifest.Permission.WriteExternalStorage)
+                != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions((Android.App.Activity) 
+                    Application.Context, new string[] { Manifest.Permission.WriteExternalStorage }, 1);
+            }
+
+            if (Android.OS.Environment.IsExternalStorageEmulated)
+            {
+                root = Android.OS.Environment.ExternalStorageDirectory.ToString();
+            }
+            else
+            {
+                root = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            }
+
+            Java.IO.File myDir = new Java.IO.File(root + "/Syncfusion");
+            myDir.Mkdir();
+
+            Java.IO.File file = new Java.IO.File(myDir, fileName);
+
+            if (file.Exists())
+            {
+                file.Delete();
+            }
         }
     }
 }
