d1c0923128e53fbc6fc080dfa519df66b4eb1c27
Paweł Idzikowski <iddziku@gmail.com>
2020-04-22 01:07:31 +0200

    Remove unnecessary usings in TagsPage

--- docAndCom_sol/docAndCom/docAndCom/TagsPage.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/TagsPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using docAndCom.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
