174dce92020004faa3b4af7a84a2bb4f945edf6b
Paweł Idzikowski <iddziku@gmail.com>
2020-04-23 12:38:07 +0200

    Create PdfViewModel.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/ViewModels/PdfViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace docAndCom.ViewModels
+{
+    public class PdfViewModel
+    {
+        public FileStream Stream { get; set; }
+
+        public string Path { get; set; }
+    }
+}
