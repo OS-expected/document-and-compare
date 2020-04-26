e78a6a3f776e319cb0d4b1c88aade9ce4f53c158
Paweł Idzikowski <iddziku@gmail.com>
2020-04-26 15:55:53 +0200

    Create Preference.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/Models/Preference.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace docAndCom.Models
+{
+    public class Preference
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
