01a275025387962cbd5091ad5ab9db9dc2b27305
Paweł Idzikowski <iddziku@gmail.com>
2020-04-26 19:19:25 +0200

    Create ShortenInvokes.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/Helpers/ShortenInvokes.cs
@@ -0,0 +1,10 @@
+﻿namespace docAndCom.Helpers
+{
+    public static class ShortenInvokes
+    {
+        public static string GetResourceString(string key)
+        {
+            return ResourceLoader.Instance.GetString(key);
+        }
+    }
+}
