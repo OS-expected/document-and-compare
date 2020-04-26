8172cbb94f9678f1bf03ce311398e52b04b624e5
Paweł Idzikowski <iddziku@gmail.com>
2020-04-26 15:56:38 +0200

    Update App.xaml.cs

--- docAndCom_sol/docAndCom/docAndCom/App.xaml.cs
+++ docAndCom_sol/docAndCom/docAndCom/App.xaml.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using docAndCom.Models;
+using docAndCom.Resources;
+using System.Globalization;
+using System.Linq;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace docAndCom
 {
     public partial class App : Application
     {
         public static string DB_PATH = string.Empty;
+
+        public static ResourceLoader ResLoader;
 
         public App()
         {
@@ -21,6 +25,10 @@ namespace docAndCom
 
             DB_PATH = DB_Path;
 
+            ResLoader = new ResourceLoader(AppResources.ResourceManager);
+
+            LoadLanguage();
+
             MainPage = new NavigationPage(new MainPage());
         }
 
@@ -34,6 +42,37 @@ namespace docAndCom
 
         protected override void OnResume()
         {
+        }
+
+        private void LoadLanguage()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(DB_PATH))
+            {
+                conn.CreateTable<Preference>();
+
+                var res = conn.Table<Preference>().SingleOrDefault(p => p.Key == "lang");
+
+                if (res != null)
+                {
+                    var newCulture = new CultureInfo(res.Value, false);
+
+                    ResLoader.SetCultureInfo(newCulture);
+                }
+                else
+                {
+                    Preference pref = new Preference()
+                    {
+                        Key = "lang",
+                        Value = "en-US"
+                    };
+
+                    conn.Insert(pref);
+
+                    var defaultCulture = new CultureInfo("en-US", false);
+
+                    ResLoader.SetCultureInfo(defaultCulture);
+                }
+            }
         }
     }
 }
