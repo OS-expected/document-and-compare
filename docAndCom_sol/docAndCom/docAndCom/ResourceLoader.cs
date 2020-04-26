1c4796ebfa30148ea7e3b44efbd0fcf3c9abde56
Paweł Idzikowski <iddziku@gmail.com>
2020-04-26 15:55:57 +0200

    Create ResourceLoader.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/ResourceLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+
+namespace docAndCom
+{
+    public class ResourceLoader : INotifyPropertyChanged
+    {
+        private readonly ResourceManager manager;
+        private CultureInfo cultureInfo;
+
+        public ResourceLoader(ResourceManager resourceManager)
+        {
+            manager = resourceManager;
+            Instance = this;
+            cultureInfo = CultureInfo.CurrentUICulture;
+        }
+
+        public static ResourceLoader Instance { get; private set; }
+
+        public string GetString(string resourceName)
+        {
+            string stringRes = this.manager.GetString(resourceName, this.cultureInfo);
+            return stringRes;
+        }
+
+        public string this[string key] => this.GetString(key);
+
+        public void SetCultureInfo(CultureInfo cultureInfo)
+        {
+            this.cultureInfo = cultureInfo;
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
