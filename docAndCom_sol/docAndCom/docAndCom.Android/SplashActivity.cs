90c98f9a4eb1f8c1db4413fa3c796231b10ef431
Paweł Idzikowski <iddziku@gmail.com>
2020-04-29 22:52:09 +0200

    Move SplashActivity, add label and icon to it

---
+++ docAndCom_sol/docAndCom/docAndCom.Android/SplashActivity.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace docAndCom.Droid
+{
+    [Activity(Label = "docAndCom", Icon = "@mipmap/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+    }
+}
