c95e7ea9f71e058b7a79fb950487dea29d66a8d9
Paweł Idzikowski <iddziku@gmail.com>
2020-07-11 18:07:18 +0200

    Create TagViewModel.cs

---
+++ docAndCom_sol/docAndCom/docAndCom/ViewModels/TagViewModel.cs
@@ -0,0 +1,11 @@
+﻿using docAndCom.Models;
+
+namespace docAndCom.ViewModels
+{
+    class TagViewModel : Tag
+    {
+        public int PhotoCount { get; set; }
+
+        public bool IsEmpty => PhotoCount > 0 ? false : true;
+    }
+}
